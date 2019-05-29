756df13273f7cd261ad19dd69960f68d6c8b5401
Philip <30839200+adavier@users.noreply.github.com>
2019-05-29 20:08:45 +0100

    Added html version using pandoc

--- process.sh
+++ process.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+echo "Compiling Series"
 out=TGWTS.md
 rm -f "$out"
 for f in Series/*.md
@@ -5,3 +7,10 @@ do
     cat "$f" >> "$out"
     echo >> "$out"
 done
+if hash pandoc 2>/dev/null; then
+	echo "Running Pandoc"
+	pandoc -s --self-contained -f gfm --toc -c ./HTML/github-custom.css --metadata pagetitle="Top Gear What's That Song?" "./TGWTS.md" -o ./TGWTS.html
+else
+	echo "No Pandoc. Not making html"
+fi
+echo "Done"
